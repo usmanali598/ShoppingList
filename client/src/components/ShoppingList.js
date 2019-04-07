49e3ad8c38022a0d5b4aa2e0569b18eb3305ea46
usman <usman_pervaiz@live.com>
2019-04-07 14:38:00 +0300

    proxy added to fix bug

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { ListGroup, ListGroupItem, Button, Container } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
