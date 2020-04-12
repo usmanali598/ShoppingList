f64f7fc40aca5735c6785d5e56a776e4f7794f14
Usman Ali <usman_pervaiz@live.com>
2020-04-12 23:06:16 +0300

    local db & heroku setup

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://user:password@ds133256.mlab.com:33256/mern-shopping-list'
+    mongoURI: 'mongodb://localhost:27017/items'
+    // mongoURI: 'mongodb://usman:mern-123-stack@ds133256.mlab.com:33256/mern-shopping-list'
 }
--- server.js
+++ server.js
@@ -20,6 +20,17 @@ mongoose
 
 app.use( '/api/items', items );
 
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    //Static folder setting
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
 const port = process.env.PORT || 5000;
 
 app.listen( port, () => console.log( 'server is on port: ' + port ) )
